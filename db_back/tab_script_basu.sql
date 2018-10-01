7c28177997fc6152cabf74dc7842e02fdaf1047e
BasanagoudaPatilGIT <basupatil.git.pro@gmail.com>
2018-10-01 18:40:34 +0530

    commited by Basu 01102018 - 2

--- db_back/tab_script_basu.sql
+++ db_back/tab_script_basu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 29, 2018 at 01:43 PM
+-- Generation Time: Oct 01, 2018 at 01:09 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
@@ -63,7 +63,13 @@ INSERT INTO `tab_basic` (`user_id`, `browser_name`, `last_access`, `id`) VALUES
 ('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 10', '2018-09-28 09:37:49', 20),
 ('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 10', '2018-09-29 06:05:12', 21),
 ('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 10', '2018-09-29 06:27:57', 22),
-('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 10', '2018-09-29 07:23:00', 23);
+('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 10', '2018-09-29 07:23:00', 23),
+('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 7', '2018-09-30 08:00:40', 24),
+('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 7', '2018-09-30 12:23:45', 25),
+('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 7', '2018-09-30 15:32:15', 26),
+('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 10', '2018-10-01 12:41:17', 27),
+('Nagamma yoga', 'Chrome;69.0.3497.100;;Windows 10', '2018-10-01 12:52:43', 28),
+('Basanagouda Patil', 'Chrome;69.0.3497.100;;Windows 10', '2018-10-01 12:55:35', 29);
 
 -- --------------------------------------------------------
 
@@ -137,7 +143,7 @@ INSERT INTO `tab_country` (`id`, `country_name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `tab_parents` (
-  `id` int(50) DEFAULT NULL,
+  `id` int(50) NOT NULL DEFAULT '0',
   `status` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_type` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `first_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -152,16 +158,9 @@ CREATE TABLE IF NOT EXISTS `tab_parents` (
   `profession` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
   `displaypicture` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `student_id` int(50) DEFAULT NULL,
-  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `tab_parents`
---
-
-INSERT INTO `tab_parents` (`id`, `status`, `user_type`, `first_name`, `middle_name`, `last_name`, `email`, `date_of_birth`, `gender_id`, `phone`, `blood_group_id`, `qualification`, `profession`, `displaypicture`, `student_id`, `created_date`) VALUES
-(1, 'Active', 'Parents', 'Shivaaaa', 'Hanumanthhhh', 'Singhhh', 'shiva@gmail.commm', '1978-10-26', 2, '666666666644', '2', 'B.B.A', 'Farmerrrrr', 'avathar.jpg', 1, '2018-09-24 14:34:04'),
-(2, 'Active', '5', 'Rakesh', 'H', 'Rao', 'rakesh@gmail.com', '2018-09-26', 1, '7032824471', '2', 'B.A', 'former', 'animals-admin.jpg', 2, '2018-09-25 13:22:11');
 
 -- --------------------------------------------------------
 
@@ -218,26 +217,28 @@ CREATE TABLE IF NOT EXISTS `tab_registration` (
 --
 
 INSERT INTO `tab_registration` (`id`, `first_name`, `middle_name`, `last_name`, `user_type`, `email_id`, `username`, `password`, `ent_id`, `created_date`, `status`, `img_name`, `mobile_no`, `address`, `gender`, `imei`, `pc_login`, `mobi_login`) VALUES
-(1, 'Basanagouda', 'D', 'Patil', '1', 'basupatil71@gmail.com', 'Basanagouda', 'NzI1OTk5OTI4Mg==', 1, '2018-07-31 10:56:03', 'Active', 'IMG11.jpg', '7259999282', 'H.No 336/2C Neelambhika Nivas\r\nQuality Buildings Panth nagar \r\nPanth Balekundri BK Belgaum - 591103\r\nState - Karnataka', 0, 0, 1, 0),
-(2, 'Rani', 'Shiva', 'singh', '3', 'rani@gmail.com', 'rani123', 'MTIzNDU2Nzg=', 2, '2018-09-24 14:34:04', 'Active', 'img1.jpg', '8889990000', '#33,JP Nagar Bangalore', 2, 0, 0, 0),
-(3, 'Shiva', 'Hanumanth', 'Singh', '5', 'shiva@gmail.com', 'shiva123', 'MTIzNDU2Nzg=', 3, '2018-09-24 14:34:04', 'Active', 'maleimg3.jpg', '6666666666', '#33,JP Nagar Bangalore', 1, 0, 0, 0),
-(4, 'Savita', 'V', 'gouda', '4', 'savita@gmail.com', 'savita123', 'MTIzNDU2Nzg=', 4, '2018-09-24 17:21:23', 'Active', 'img1.jpg', '3333333333', '#13,Raadev galli,Shapur', 2, 0, 0, 0),
-(5, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 5, '2018-09-25 11:12:53', 'Active', 'chicken.jpg', '5555566666', '#45,RR street,Goa', 2, 0, 0, 0),
-(6, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 6, '2018-09-25 11:20:05', 'Active', 'animals.jpg', '5555566666', '#45,RR street,Goa', 2, 0, 0, 0),
-(7, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 7, '2018-09-25 11:27:13', 'Active', 'animals.jpg', '5555566666', '#45,RR street,Goa', 2, 0, 0, 0),
-(8, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 8, '2018-09-25 11:29:51', 'Active', 'animals.jpg', '5555566666', '#45,RR street,Goa', 2, 0, 0, 0),
-(9, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 9, '2018-09-25 11:35:38', 'Active', 'animals.jpg', '5555566666', '#45,RR street,Goa', 1, 0, 0, 0),
-(10, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 10, '2018-09-25 11:37:28', 'Active', 'animals.jpg', '5555566666', '#45,RR street,Goa', 2, 0, 0, 0),
-(11, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 11, '2018-09-25 11:47:17', 'Active', 'chicken.jpg', '5555566666', '#45,RR street,Goa', 2, 0, 0, 0),
-(12, 'Rosy', 'Mario', 'Brigyanza', '4', 'rosy@gmail.com', 'rosy123', 'MTIzNDU2Nzg=', 12, '2018-09-25 11:53:24', 'Active', 'animals.jpg', '5555566666', '#45,RR street,Goa', 2, 0, 0, 0),
-(13, 'gg', 'ggg', 'gg', '4', 'ggg@gmail.com', 'sadfgd', 'MTIzNDU2Nzg=', 13, '2018-09-25 12:01:41', 'Active', 'animals.jpg', '7032824471', 'bdfhfd', 1, 0, 0, 0),
-(14, 'Raju', 'Rakesh', 'Rao', '3', 'raju@gmail.com', 'raju123', 'MTIzNDU2Nzg=', 14, '2018-09-25 13:22:11', 'Active', 'animals.jpg', '7032824471', '#8-326,Alwyn Colony, Miyapur', 1, 0, 0, 0),
-(15, 'Rakesh', 'H', 'Rao', '5', 'rakesh@gmail.com', 'rakesh123', 'MTIzNDU2Nzg=', 15, '2018-09-25 13:22:11', 'Active', 'animals-admin.jpg', '7032824471', '#8-326,Alwyn Colony, Miyapur', 1, 0, 0, 0),
-(16, 'Sangeeta', 'Z', 'Corn', '4', 'sangeeta@gmail.com', 'sangeeta123', 'MTIzNDU2Nzg=', 16, '2018-09-25 13:26:04', 'Active', 'goat.jpg', '7032824445', '#12,lalbhag Mumbai', 2, 0, 0, 0),
-(17, 'Sunita', 'Ashwin', 'Kooli', '4', 'sunita@gmail.com', 'sunita123', 'MTIzNDU2Nzg=', 17, '2018-09-25 13:31:56', 'Active', 'grass.jpg', '4443332221', '#121,RB Road,Hasan', 2, 0, 0, 0),
-(18, 'Ashwin', 'Udaya', 'Patil', '4', 'ashwin@gmail.com', 'ashwin123', 'MTIzNDU2Nzg=', 18, '2018-09-25 13:36:53', 'Active', 'animals-admin.jpg', '5555566666', '#34,yagna building, Kodagu', 1, 0, 0, 0),
-(19, 'Veena', 'C', 'ZA', '4', 'veena@hm.com', 'veena123', 'MTIzNDU2Nzg=', 19, '2018-09-27 15:17:02', 'Active', 'Capture.jpg', '7777777777', 'dgdtdtyt', 2, 0, 0, 0),
-(20, 'Vidya', 'Jai', 'simha', '4', 'vidya@gmail.com', 'vidya123', 'MTIzNDU2Nzg=', 20, '2018-09-27 15:24:07', 'Active', 'Capture.jpg', '5555566666', '43,fdgdsff', 2, 0, 0, 0);
+(1, 'Basanagouda', 'D', 'Patil', '1', 'basupatil71@gmail.com', 'Basanagouda', 'NzI1OTk5OTI4Mg==', 1, '2018-07-31 10:56:03', 'Active', 'IMG11.jpg', '7259999282', 'H.No 336/2C Neelambhika Nivas\r\nQuality Buildings Panth nagar \r\nPanth Balekundri BK Belgaum - 591103\r\nState - Karnataka', 0, 0, 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tab_route_mapping`
+--
+
+CREATE TABLE IF NOT EXISTS `tab_route_mapping` (
+  `id` int(10) DEFAULT NULL,
+  `vehicle_id` int(10) DEFAULT NULL,
+  `route_name` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tab_route_mapping`
+--
+
+INSERT INTO `tab_route_mapping` (`id`, `vehicle_id`, `route_name`) VALUES
+(1, 2, 'Ashok Nagar'),
+(2, 3, 'Mahantesh nagar');
 
 -- --------------------------------------------------------
 
@@ -253,7 +254,7 @@ CREATE TABLE IF NOT EXISTS `tab_section` (
   `class_capacity` int(20) DEFAULT NULL,
   `class_number` int(20) DEFAULT NULL,
   `class_location` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `class_section` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -263,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `tab_section` (
 --
 
 INSERT INTO `tab_section` (`id`, `count`, `status`, `class_id`, `class_capacity`, `class_number`, `class_location`, `created_date`, `class_section`) VALUES
-(1, 3, 'Active', 1, 30, 101, '1st Floor', '2018-09-01 05:34:13', 'A'),
+(1, 1, 'Active', 1, 30, 101, '1st Floor', '2018-10-01 18:32:29', 'A'),
 (2, 1, 'Active', 1, 30, 102, '1st Floor', '2018-09-01 05:34:13', 'B'),
 (3, 1, 'Active', 2, 30, 103, '1st Floor', '2018-09-01 05:34:13', 'A'),
 (4, 1, 'Active', 2, 30, 104, '1st Floor', '2018-09-01 05:34:13', 'B'),
@@ -303,7 +304,8 @@ CREATE TABLE IF NOT EXISTS `tab_series` (
 --
 
 INSERT INTO `tab_series` (`id`, `user_type`, `count`, `changed_datetime`) VALUES
-(1, 4, 1005, '2018-09-27 15:24:07');
+(1, 4, 1005, '2018-09-27 15:24:07'),
+(2, 3, 1, '2018-10-01 18:25:03');
 
 -- --------------------------------------------------------
 
@@ -334,7 +336,7 @@ CREATE TABLE IF NOT EXISTS `tab_students` (
   `id` int(50) NOT NULL,
   `status` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_type` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `admission_no` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `admission_no` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `first_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `middle_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `last_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -356,18 +358,13 @@ CREATE TABLE IF NOT EXISTS `tab_students` (
   `roll_num` int(50) DEFAULT NULL,
   `class_id` int(20) DEFAULT NULL,
   `section_id` int(10) NOT NULL,
+  `route_id` int(10) DEFAULT NULL,
+  `pickup_time` time DEFAULT NULL,
+  `drop_time` time DEFAULT NULL,
   `displaypicture` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `tab_students`
---
-
-INSERT INTO `tab_students` (`id`, `status`, `user_type`, `admission_no`, `first_name`, `middle_name`, `last_name`, `email`, `date_of_birth`, `gender_id`, `address`, `city`, `country_id`, `zipcode`, `per_address`, `per_city`, `per_country_id`, `per_zipcode`, `phone`, `blood_group_id`, `date_of_joining`, `date_of_leaving`, `roll_num`, `class_id`, `section_id`, `displaypicture`, `created_date`) VALUES
-(1, 'Active', '3', '2018-101', 'Raniii', 'Shivaaa', 'singhhh', 'rani@gmail.comm', '1998-12-19', 1, '#33,JP Nagar Bangaloreeeeeeee', 'Bangaloreee', 1, 530001, '#33,JP Nagar Bangaloreeeee', 'Bangaloreee', 1, '530001', '8889990055', '1', '2011-06-22', '0000-00-00', 2, 1, 1, 'corn.jpg', '2018-09-24 14:34:04'),
-(2, 'Active', '3', '2018-102', 'Raju', 'Rakesh', 'Rao', 'raju@gmail.com', '2018-09-04', 1, '#8-326, Alwyn Colony, Miyapur', 'HYDERABAD', 1, 500049, '#8-326,Alwyn Colony, Miyapur', 'HYDERABAD', 1, '500049', '7032824471', '1', '2018-09-25', '0000-00-00', 1, 1, 1, 'animals.jpg', '2018-09-25 13:22:11');
 
 -- --------------------------------------------------------
 
@@ -481,12 +478,8 @@ INSERT INTO `tab_teacher_assignment` (`id`, `class_id`, `section_id`, `teacher_i
 
 CREATE TABLE IF NOT EXISTS `tab_transportation` (
   `id` int(10) NOT NULL,
-  `vehicle_route` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `vehicle_number` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
   `vehicle_capacity` int(10) DEFAULT NULL,
-  `student_id` int(10) DEFAULT NULL,
-  `pick_up_time` time DEFAULT NULL,
-  `droppin_ time` time DEFAULT NULL,
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `region_id` int(10) NOT NULL,
   UNIQUE KEY `id` (`id`)
@@ -496,8 +489,10 @@ CREATE TABLE IF NOT EXISTS `tab_transportation` (
 -- Dumping data for table `tab_transportation`
 --
 
-INSERT INTO `tab_transportation` (`id`, `vehicle_route`, `vehicle_number`, `vehicle_capacity`, `student_id`, `pick_up_time`, `droppin_ time`, `created_date`, `region_id`) VALUES
-(1, '', 'KA-1234', 100, NULL, NULL, NULL, '2018-09-28 18:25:15', 1);
+INSERT INTO `tab_transportation` (`id`, `vehicle_number`, `vehicle_capacity`, `created_date`, `region_id`) VALUES
+(1, 'KA-1234', 100, '2018-09-28 18:25:15', 1),
+(2, 'MH-3456', 50, '2018-09-30 12:09:34', 2),
+(3, 'BA-6666', 40, '2018-09-30 12:13:53', 3);
 
 -- --------------------------------------------------------
 
